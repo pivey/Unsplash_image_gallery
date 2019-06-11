d519bf34d2e8afbde08ae036f3e7ad9eb58c15ce
Camilla Stivenson <camilla.stivenson@appliedtechnology.se>
2019-06-11 11:30:52 +0200

    css changes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import './style.scss';
+import {
+  className
+} from 'postcss-selector-parser';
+console.log('HI')
+
+document.querySelector('.container').addEventListener('click', (e) => {
+        if (e.target.parentNode.tagName === 'DIV') {
+        if (e.target.className === '') {
+          e.target.parentNode.className= 'tall';
+        } else if (e.target.parentNode.className === 'tall') {
+            e.target.parentNode.className= 'long';
+        //   e.target.parentNode.classList.remove('tall');
+        //   e.target.parentNode.classList.add('long');
+        } else if (e.target.parentNode.className === 'long') {
+            e.target.parentNode.className= 'large';
+        //   e.target.parentNode.classList.remove('long');
+        //   e.target.parentNode.classList.add('large');
+        } else if (e.target.parentNode.className === 'large') {
+            e.target.parentNode.className= '';
+        //   e.target.parentNode.classList.remove('large');
+        //   e.target.parentNode.classList.add('');
+        }
+      }
+});
+
--- webpack.config.js
+++ webpack.config.js
@@ -8,18 +8,14 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   module: {
-    rules: [{
-      test: /\.scss$/,
-      use: [
-        "style-loader",
-        "css-loader",
-        "sass-loader"
-      ]
-    }]
-    // devServer: {
-    //   contentBase: path.join(__dirname, 'dist'),
-    //   compress: true,
-    //   port: 9000
-    // }
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
   }
 };
